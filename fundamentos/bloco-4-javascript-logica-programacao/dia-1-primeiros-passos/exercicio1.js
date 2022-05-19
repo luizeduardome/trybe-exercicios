1a197ba8be417de3f799b8f48b75efa8c347229b
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-19 16:06:12 -0300

    Criado arquivo html para linkar com arquivo JS e executado comandos do vídeo 2.

---
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/exercicio1.js
@@ -0,0 +1,8 @@
+const myName = 'Luiz Eduardo';
+const birthCity = 'Tenesse';
+const birthYear = 1500;
+console.log(myName, birthCity, birthYear)
+const birthYear = 2030;
+console.log(birthYear);
+const birthCity = 'Amazonas';
+console.log(birthCity);
--- fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
@@ -1,5 +1,28 @@
-let age = 20;
-let name = 'Hulk';
+// VÍDEO 1 
 
-console.log(age);
-console.log(name);
+// let age = 20;
+// let name = 'Hulk';
+
+// console.log(age);
+// console.log(name);
+
+// VÍDEO 2 
+let movie = "Avengers"; //string literal
+let score = 10; //numer literal
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //redefinição
+
+//+, -, *, /, **
+
+let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+salary++;
+
+console.log(salary);
