86a4de6c8cfdf5fd70addb7d60f36580358c52a4
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-19 17:54:06 -0300

    Criado arquivo do exercício 2 e solucionado as atividade do vídeo 3.

---
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/exercicio2.js
@@ -0,0 +1,23 @@
+/* 🚀 Agora que você já conhece os operadores aritméticos básicos do JavaScript, vamos praticá-los? 
+Você pode consultar a lista de operadores na tabela JavaScript Arithmetic Operators disponível 
+nesse link se tiver dúvidas. Vamos fazer algumas operações simples para encontrarmos a área e o 
+perímetro de um retângulo de base 5 e altura 8. 
+
+Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. 
+Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+*/
+
+
+const base = 5;
+let height = 8;
+const area = (base * height);
+console.log(area);
+
+
+const lado1 = 30;
+const lado2 = 25;
+const lado3 = 30;
+const perimeter = (lado1 + lado2 + lado3);
+console.log(perimeter)
--- fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
@@ -17,12 +17,34 @@ let color = null; //redefinição
 
 let salary = 3500;
 
-// console.log(salary + salary);
-// console.log(salary - salary);
-// console.log(salary * salary);
-// console.log(salary / salary);
-// console.log(3 ** 3);
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(3 ** 3);
 
 salary++;
 
 console.log(salary);
+
+// VÍDEO 3
+
+/* Agora é a hora da mão na massa no código! No exemplo abaixo, temos as informações de um paciente. 
+Utilize o operador typeof para imprimir qual o tipo das variáveis patientId, isEnrolled, patientInfo e patientEmail.
+Esse operador retorna qual o tipo de uma variável, objeto, função ou expressão. Exemplo: 
+console.log(typeof patientId) retornará number. */
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+    firstName: "Ana",
+    lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientId);
+// console.log(typeof patientEmail);
+console.log(typeof patientAge);
+console.log(typeof patientId);
