538bdaf36805b53ad6843f880e830bc2e1e7eb64
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-20 17:50:42 -0300

    Adicionado exercício do operador AND ao arquivo script.js

--- fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-1-primeiros-passos/scripts.js
@@ -74,3 +74,76 @@ else if (trybe >= 19.4 && trybe < 20) {
 else {
     console.log("Fora do momento síncrono.");
 }
+
+// Operadores lógicos
+const comida = 'pão na chapa';
+const bebida = 'cafezinho';
+
+if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+    console.log("Muito obrigado pela refeição :)");
+} else {
+    console.log("Acho que houve um engano com o meu pedido.");
+}
+
+
+// Precisamos manter em mente que só teremos true se as duas expressões em volta dele forem true também.
+
+const conditionONe = true;
+const conditionTwo = false;
+
+console.log(conditionONe && conditionTwo);
+
+// Mas... E o que acontece se existir um encadeamento de &&? 🤔
+
+const cenouras = true;
+const leite = true;
+const arroz = true;
+const feijao = true;
+
+const listaDeCompras = cenouras && leite && arroz && feijao;
+console.log(listaDeCompras);
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+
+// 1 - Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, 
+// para representar as horas do dia.
+
+const currentHour = 13;
+
+// Crie uma variável chamada "message" que, inicialmente, é uma string vazia.
+let message = " ";
+
+// Implemente condicionais para que:
+
+// Se o horário for maior ou igual a 22, insira "Não deveríamos comer nada, é hora de dormir" na variável "message".
+
+// Se o horário for maior ou igual a 18 e menor que 22, insira "Rango da noite, vamos jantar :D" na variável "message".
+
+// Se o horário for maior ou igual a 14 e menor que 18, insira "Vamos fazer um bolo pro café da tarde?" na variável "message".
+
+// Se o horário estiver entre 11 e 14, insira "Hora do almoço!!!" na variável "message".
+
+// Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour > 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+} else if (currentHour > 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém passado";
+} else {
+    message = 0;
+}
+
+console.log(message);
+
+// VÍDEO 5
+
