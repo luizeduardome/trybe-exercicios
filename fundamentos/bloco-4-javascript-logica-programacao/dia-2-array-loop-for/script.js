67a3bdb90060fbe2c005285532ffa3142ce32fa4
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-21 16:19:23 -0300

    Criado o arquivo script.js da aula 2.

---
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-2-array-loop-for/script.js
@@ -0,0 +1,9 @@
+let pizzas = ['Frango', 'Calabreza', 'Palmito', 'Atum'];
+pizzas.push('Chocolate');
+
+console.log(pizzas); // imprime o meu array
+console.log(pizzas.length); // imprime o tamanho do meu array
+console.log(pizzas.sort()); // imprime o meu array em ordem alfabética
+console.log(pizzas[3]);// imprime o meu array, apenas o índice que necessito
+
+
