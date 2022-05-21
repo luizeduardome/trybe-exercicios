7aaa663873a2dd62a9bed76df2f060c39dfe599e
Luiz Eduardo <airmaesterdev@outlook.com>
2022-05-21 18:13:28 -0300

    Feito as práticas de 1 a 5 e o exercício 1.

--- fundamentos/bloco-4-javascript-logica-programacao/dia-2-array-loop-for/script.js
+++ fundamentos/bloco-4-javascript-logica-programacao/dia-2-array-loop-for/script.js
@@ -1,9 +1,60 @@
 let pizzas = ['Frango', 'Calabreza', 'Palmito', 'Atum'];
-pizzas.push('Chocolate');
 
-console.log(pizzas); // imprime o meu array
-console.log(pizzas.length); // imprime o tamanho do meu array
-console.log(pizzas.sort()); // imprime o meu array em ordem alfabética
-console.log(pizzas[3]);// imprime o meu array, apenas o índice que necessito
+
+// pizzas.push('Chocolate');
+
+// console.log(pizzas); // imprime o meu array
+// console.log(pizzas.length); // imprime o tamanho do meu array
+// console.log(pizzas.sort()); // imprime o meu array em ordem alfabética
+// console.log(pizzas[3]);// imprime o meu array, apenas o índice que necessito
+
+
+// console.log(pizzas[1]);
+// console.log(pizzas[2]);
+
+// Ao inves de ficar imprimindo índice por índice, utilizaremos o comando 'for'
+
+for (let index = 0; index < pizzas.length; index += 1) {
+    console.log(pizzas.sort()[index]);
+}
+
+
+// Prática 1
+let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(taskList.length);
+
+// Prática 2
+let taskList2 = ['Tomar chá', 'Hora almoço', 'Brincar com o gato'];
+
+let searchForFirstTask = taskList2[0];
+console.log(searchForFirstTask);
+
+
+// Prática 3
+let taskList3 = ['Tomar banho', 'Digitar memorando', 'Enviar e-mail'];
+taskList3.push('Fazer exercícios da trybe'); // Adiciona mais uma tarefa
+console.log(taskList3);
+
+// Prática 4
+let taskList4 = ['Tomar banho', 'Digitar memorando', 'Enviar e-mail'];
+taskList4.unshift("Fazer exercícios de Stanford."); // Adiciona no início da lista
+taskList4.pop(); // remove a última tarefa
+console.log(taskList4);
+
+
+// Prática 5
+let taskList5 = ['Tomar banho', 'Digitar memorando', 'Enviar e-mail'];
+let indexOfTask = taskList5.indexOf('Digitar memorando') // Procura o índice de um ítem no Array
+console.log(indexOfTask);
+
+// EXERCÍCIO 1
+// Obtenha o valor "Serviços" do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+console.log(menuServices);
+
+
 
 
